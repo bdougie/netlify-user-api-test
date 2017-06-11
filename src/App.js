13774c5aece4266afbcf31f6950b0da05902648c
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-06-11 10:09:19 -0500

    README

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'unfetch'
 
 class App extends Component {
+  state = {
+    data: null,
+  }
+
+  componentDidMount() {
+    fetch('http://athlete-tracy-30723.netlify.com/users.json')
+      .then( r => r.json() )
+      .then( data => {
+        this.setState({data})
+      });
+  }
+
   render() {
+    console.log(this.state.data)
+    const {data} = this.state;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to a Netlify User API</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <ul>
+            {data &&
+              data.map((item) => (
+                <li style={{listStyleType: 'none'}} key={item.id}>
+                  {item.name}
+                </li>
+            ))}
+          </ul>
         </p>
       </div>
     );
